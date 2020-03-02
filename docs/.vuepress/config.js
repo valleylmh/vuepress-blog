a103e5e0113a92023a2dad3aacecc1cf1d1f4d6d
liumh <valleylmh@163.com>
2020-03-02 11:44:30 +0800

    个人静态博客网站更新文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,49 @@
+const LAOYULIFE = '老喻的人生算法课'
 module.exports = {
   // base: '/',
   dest: 'dist',
-  title: 'TypeScript 从零实现 axios',
-  description: '学习使用 TypeScript 从零实现 axios 库',
+  title: '一界码农',
+  description: '越努力越幸运',
   themeConfig: {
     editLinks: false,
     docsDir: 'docs',
-    nav: [],
-    sidebar: [
+    nav: [
+      // { text: 'Coding', link: '/coding/' },
+      // { text: '投资学习', link: '/invest/' },
       {
-        title: '初识 TypeScript',
-        collapsable: false,
-        children: [
-          ['chapter1/', 'Introduction'],
-          'chapter1/install',
-          'chapter1/start'
-        ]
+        text: '生活life', link: '/study_life/'
+        // ariaLabel: '生活不易且珍惜',
+        // items: [
+        //   { text: 'Chinese', link: '/language/chinese/' },
+        //   { text: 'Japanese', link: '/language/japanese/' }
+        // ]
       }
-    ]
-  }
+    ],
+    sidebar: {
+      '/coding/': [
+        {
+          title: '初识 TypeScript',
+          collapsable: false,
+          children: [
+            ['chapter1/', 'Introduction'],
+            'chapter1/install',
+            'chapter1/start'
+          ]
+        }
+      ],
+      '/study_life/': [
+        {
+          title: '老喻的人生算法课',
+          collapsable: false,
+          children: [
+            [LAOYULIFE+'/', '为什么推荐'],
+            LAOYULIFE + '/01_A',
+          ]
+        }
+      ]
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top'],
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,15 @@
 ---
 home: true
-heroImage: /ts-logo.png
-actionText: 开始学习 →
-actionLink: /chapter1/
+heroImage: /logo.jpg
+actionText: 个人主页 →
+actionLink: /study_life/
+features:
+- title: Coding为生
+  details: 以技术谋生、面向API编程
+- title: 终生学习者
+  details: 越努力越幸运
+- title: 理想生活
+  details: 生活的理想就是为了理想的生活
 ---
+
+<p style="text-align: center;">©2020 一界码农 &nbsp;&nbsp;&nbsp;<a target="_blank" href="http://www.beian.miit.gov.cn" style="text-decoration: none; color: inherit;">浙ICP备19050208号</a></p>
