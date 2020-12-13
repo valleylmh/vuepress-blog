51efce8d3ce74ea41d5c7a2146ec223cdafc461d
valleylmh <valleyllmh@gmail.com>
2020-12-13 11:21:07 +0800

    增模meta信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,8 @@ module.exports = {
   head: [
     ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }],
     ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover' }],
   ],
   themeConfig: {
