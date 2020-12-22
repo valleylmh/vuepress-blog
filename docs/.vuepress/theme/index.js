075aab304f1e39d51d49ce3fdff1b38b81c2feb8
valleylmh <valleyllmh@gmail.com>
2020-12-22 08:41:53 +0800

    git提交uninx时间调整

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -12,15 +12,16 @@ module.exports = {
         [
           '@vuepress/last-updated',
           {
-            dateOptions: { hour12: false }
-            // transformer: (timestamp, lang) => {
-            //   const dayjs = require('dayjs')
-            //   require('dayjs/locale/zh-cn')
-            //   dayjs.locale('zh-cn') // 全局使用
-            //   dayjs().locale('zh-cn').format() // 当前实例使用
-            //   // const time = process.env.NODE_ENV === 'development' ? dayjs(timestamp) : dayjs(timestamp).utcOffset(8)
-            //   return dayjs(timestamp).format('YYYY-MM-DD HH:mm')
-            // }
+            // dateOptions: { hour12: false }
+            transformer: (timestamp, lang) => {
+              const dayjs = require('dayjs')
+              require('dayjs/locale/zh-cn')
+              // dayjs.locale('zh-cn') // 全局使用
+              // dayjs().locale('zh-cn').format() // 当前实例使用
+              console.log('timestamp========',timestamp)
+              timestamp = process.env.NODE_ENV === 'development' ? timestamp : timestamp + 8*60*60
+              return dayjs(timestamp).format('YYYY-MM-DD HH:mm')
+            }
           }
         ]
     ]
